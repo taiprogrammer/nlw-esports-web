f3f0b49cfbd1ec9b766dab88bfe119ca988ab831
taiprogrammer <taiza.marques@sptech.school>
2022-09-12 10:37:03 -0300

    props with ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 
+interface ButtonProps {
+  title : String;
+}
+
+function Button(props: ButtonProps) {
+  return <button>{props.title}</button>
+}
+
 function App() {
 
   return (
-    <h1>My React App</h1>
+    <>
+    <Button title="Send 1"/>
+    <Button title="Send 2"/>
+    </>
   )
 }
 
